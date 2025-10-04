d33c5035b452762449e13f15e522c33c3b1b8168
David Banks <amoe@solasistim.net>
2025-10-04 06:03:34 +0100

    write spreadsheet with data

--- rewrite.py
+++ rewrite.py
@@ -5,6 +5,7 @@ import engine
 import warnings
 import json
 import openpyxl
+import audioread.exceptions
 
 # probably bad but works
 warnings.filterwarnings('ignore')
@@ -24,33 +25,35 @@ def print_probabilities(events: dict[int, list[tuple]]) -> None:
     for start, end, prob in events:
         print(f"Event {seconds_to_mmss(start)}–{seconds_to_mmss(end)} (p={prob:.2f})")        
 
+out_wb = openpyxl.Workbook(write_only=True)
+out_ws = out_wb.create_sheet()
+out_ws.append(engine.create_header_row())
+        
 for root, dirs, files in os.walk(base_dir):
     relative_root = os.path.relpath(root, base_dir)
-    for file_ in files:
+    for file_ in sorted(files):
         absolute_path = os.path.join(root, file_)
         print("Analysing:", absolute_path, flush=True)
-        probabilities = engine.detect_classes(absolute_path, threshold=0.5)
+        try:
+            probabilities = engine.detect_classes(absolute_path, threshold=0.5)
 
-        out_wb = openpyxl.Workbook(write_only=True)
-        out_ws = out_wb.create_sheet()
+            row = [absolute_path]
+            class_indices = engine.get_classes()
+            for k in class_indices:
+                v = probabilities.get(int(k))
+                if v:
+                    row.append(len(v))
+                else:
+                    row.append(0)
 
-        out_ws.append(engine.create_header_row())
-        
-        row = [absolute_path]
+            out_ws.append(row)
 
-        class_indices = engine.get_classes()
+            with open('out.json', 'w') as f:
+                json.dump(probabilities, f)
+        except audioread.exceptions.NoBackendError as e:
+            print("skipping:", absolute_path)
+            pass
 
-        for k in class_indices:
-            v = probabilities.get(int(k))
-            if v:
-                row.append(len(v))
-            else:
-                row.append(0)
-        
-        out_ws.append(row)
-        out_wb.save('out.xlsx')
-
-        with open('out.json', 'w') as f:
-            json.dump(probabilities, f)
         
 #        print_probabilities(probabilities)
+out_wb.save('out.xlsx')
